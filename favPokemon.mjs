3b0aad7bc9fe52da0c6c4843e7410a4ae93ebbc6
winstonsullivan <winstonsullivan1@gmail.com>
2024-02-01 19:24:19 -0500

    working JS

---
+++ favPokemon.mjs
@@ -0,0 +1,7 @@
+//fetch all pokemon data from pokeapi
+const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0");
+const allPokemon = await response.json();
+
+//convert allPokemon.results from array of objects to array of names
+const pokemonNames = allPokemon.results.map((element) => element.name);
+
